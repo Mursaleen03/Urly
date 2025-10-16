2cbea2afecef738d97c733c6569ee37e857d3e3b
Mursaleen <mohdmursaleen9306@gmail.com>
2025-10-16 23:57:17 +0530

    Stats

--- src/pages/redirect-link.jsx
+++ src/pages/redirect-link.jsx
@@ -1,4 +1,17 @@
+import useFetch from "@/hooks/use-fetch";
+import { getLongUrl, storeClicks } from "db/apiUrls";
+import { useParams } from "react-router-dom"
+
 const RedirectLink = () => {
+
+  const {id} = useParams();
+  const {loading, data, fn} = useFetch(getLongUrl, id);
+
+  const { loadingl: loadingStats, fn: fnStats} = useFetch(storeClicks, {
+    id: data?.id,
+    originalUrl: data?.original_url,
+  });
+
   return (
     <div>Redirect Link</div>
   )
